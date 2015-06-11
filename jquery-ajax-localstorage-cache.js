cd80271a7ab36848a9398419ff1465d4bbe3c464
Christopher Keefer <SaneMethodology@gmail.com>
2015-06-10 20:22:58 -0400

    Incorporating some changes that were made to previous version of master, and pulling generating of cache key into a separate reusable function.

--- jquery-ajax-localstorage-cache.js
+++ jquery-ajax-localstorage-cache.js
@@ -3,6 +3,13 @@
  */
 (function($){
     /**
+     * Generate the cache key under which to store the local data - either the cache key supplied,
+     * or one generated from the url, the type and, if present, the data.
+     */
+    var genCacheKey = function(options){
+        return options.cacheKey || options.url.replace(/jQuery.*/,'') + options.type + (options.data || '');
+    };
+    /**
      * Prefilter for caching ajax calls.
      * See also $.ajaxTransport for the elements that make this compatible with jQuery Deferred.
      * New parameters available on the ajax call:
@@ -17,7 +24,7 @@
     $.ajaxPrefilter(function(options){
         var storage = (options.localCache === true) ? window.localStorage : options.localCache,
             hourstl = options.cacheTTL || 5,
-            cacheKey = options.cacheKey || options.url.replace(/jQuery.*/,'') + options.type + options.data,
+            cacheKey = genCacheKey(options),
             ttl = storage.getItem(cacheKey + 'cachettl'),
             cacheValid = options.isCacheValid,
             value;
@@ -29,28 +36,28 @@
         }
 
         if (ttl && ttl < +new Date()){
-            storage.removeItem( cacheKey );
-            storage.removeItem( cacheKey + 'cachettl' );
-            ttl = 'expired';
+            storage.removeItem(cacheKey);
+            storage.removeItem(cacheKey + 'cachettl');
+            ttl = 0;
         }
 
-        value = storage.getItem( cacheKey );
+        value = storage.getItem(cacheKey);
         if (!value){
             // If it not in the cache, we store the data, add success callback - normal callback will proceed
             if (options.success) {
                 options.realsuccess = options.success;
             }
-            options.success = function( data ) {
+            options.success = function(data) {
                 var strdata = data;
-                if (this.dataType.indexOf('json') === 0) strdata = JSON.stringify(data);
+                if (this.dataType.toLowerCase().indexOf('json') === 0) strdata = JSON.stringify(data);
 
                 // Save the data to storage catching exceptions (possibly QUOTA_EXCEEDED_ERR)
                 try {
-                    storage.setItem( cacheKey, strdata );
+                    storage.setItem(cacheKey, strdata);
                 } catch (e) {
                     // Remove any incomplete data that may have been saved before the exception was caught
-                    storage.removeItem( cacheKey );
-                    storage.removeItem( cacheKey + 'cachettl' );
+                    storage.removeItem(cacheKey);
+                    storage.removeItem(cacheKey + 'cachettl');
                     console.log('Cache Error:'+e, cacheKey, strdata );
                 }
 
@@ -58,8 +65,8 @@
             };
 
             // store timestamp
-            if ( ! ttl || ttl === 'expired' ) {
-                storage.setItem( cacheKey + 'cachettl', +new Date() + 1000 * 60 * 60 * hourstl );
+            if (!ttl){
+                storage.setItem(cacheKey + 'cachettl', +new Date() + 1000 * 60 * 60 * hourstl);
             }
         }
     });
@@ -74,14 +81,14 @@
     $.ajaxTransport("+*", function(options){
         if (options.localCache)
         {
-            var cacheKey = options.cacheKey || options.url.replace(/jQuery.*/,'') + options.type + options.data,
+            var cacheKey = genCacheKey(options),
                 storage = (options.localCache === true) ? window.localStorage : options.localCache,
                 value = (storage) ? storage.getItem(cacheKey) : false;
 
             if (value){
                 // In the cache? Get it, parse it to json if the dataType is JSON,
                 // and call the completeCallback with the fetched value.
-                if (options.dataType.indexOf('json') === 0) value = JSON.parse(value);
+                if (options.dataType.toLowerCase().indexOf('json') === 0) value = JSON.parse(value);
                 return {
                     send: function(headers, completeCallback) {
                         var response = {};
